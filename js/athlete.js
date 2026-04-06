10eed11ba60562e675a4a0b1e64cd7ad7b70ab55
Brent Maxwell <bmax1117@gmail.com>
2026-04-06 14:10:36 -0500

    fix correct google csv

--- js/athlete.js
+++ js/athlete.js
@@ -2,7 +2,6 @@
 // CONFIG
 // ===============================
 const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS81ri1sMtpBVl605PVV_Te2WdA3hVohdXIb1Lc22CrUJSdzXUzGa-0Z0THGtlSa9WVaa77owi-_BAR/pub?output=csv";
-
 // ===============================
 // INIT
 // ===============================
