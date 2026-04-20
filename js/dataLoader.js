0d7dfa1e34401efbf9946be91d4f9a6bb1a54022
Elite Athletic Performance <bmax1117@gmail.com>
2026-04-20 07:17:32 -0500

    Update dataLoader.js

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -19,7 +19,7 @@ async function loadCSV(url) {
 let APP_DATA = [];
 
 async function loadAthleteData() {
-  const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS81ri1sMtpBVl605PVV_Te2WdA3hVohdXIb1Lc22CrUJSdzXUzGa-0Z0THGtlSa9WVaa77owi-_BAR/pub?output=csv";
+  const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS81ri1sMtpBVl605PVV_Te2WdA3hVohdXIb1Lc22CrUJSdzXUzGa-0Z0THGtlSa9WVaa77owi-_BAR/pub?output=csv&t=" + Date.now()";
 
   const raw = await loadCSV(url);
 
