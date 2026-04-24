c9b05fc2b1642946c033698fec1e94a933632b28
Elite Athletic Performance <bmax1117@gmail.com>
2026-04-24 09:34:30 -0500

    Update themeLoader.js

--- js/themeLoader.js
+++ js/themeLoader.js
@@ -49,7 +49,7 @@ window.APP_READY = new Promise(async (resolve, reject) => {
         logo: base + "images/roosters-logo.png",
 
         dataURL: "https://script.google.com/macros/s/AKfycbyZPnii9Qf3VdDiTRn1tPt_BOnIv22m2r5n5afnEQ6pNGO2sWO-jQa6MBtJBNfYGyA/exec",
-        submitURL: "https://script.google.com/macros/s/AKfycbyZPnii9Qf3VdDiTRn1tPt_BOnIv22m2r5n5afnEQ6pNGO2sWO-jQa6MBtJBNfYGyA/exec"
+        submitURL: "https://script.google.com/macros/s/AKfycbyZPnii9Qf3VdDiTRn1tPt_BOnIv22m2r5n5afnEQ6pNGO2sWO-jQa6MBtJBNfYGyA/exec" 
       }
     };
 
