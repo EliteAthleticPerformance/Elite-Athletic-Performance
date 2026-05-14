8d37407e0de622de5f8ab42977714a5018803c03
Elite Athletic Performance <bmax1117@gmail.com>
2026-05-13 19:17:40 -0500

    Create schoolConfig.js

---
+++ js/schoolConfig.js
@@ -0,0 +1,31 @@
+// ========================================
+// SCHOOL CONFIG
+// ========================================
+
+window.SCHOOL_CONFIG = {
+
+  jefferson: {
+    school: "Jefferson",
+
+    active: true,
+
+    trial: true,
+
+    subscriptionStatus: "trial",
+
+    trialStart: "2026-05-14",
+
+    trialEnd: "2026-05-28"
+  },
+
+  pleasanthill: {
+    school: "Pleasant Hill",
+
+    active: true,
+
+    trial: false,
+
+    subscriptionStatus: "paid"
+  }
+
+};
