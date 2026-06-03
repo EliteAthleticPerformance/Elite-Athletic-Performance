e1da0d6068159220d9877b54c631a6b8836fee6d
Elite Athletic Performance <bmax1117@gmail.com>
2026-06-03 15:26:15 -0500

    Update speed.js

--- js/speed.js
+++ js/speed.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", async () => {
 
+  const data = await loadAthleteData();
+
+  const uniqueWeeks = [
+    ...new Set(
+      data
+        .map(r => r.testDate)
+        .filter(Boolean)
+    )
+  ];
+
+  console.log(uniqueWeeks);
+
+});
